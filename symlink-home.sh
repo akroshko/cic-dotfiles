33ec86d7454e3dd09f9ed2de4dc6845659be0d2f
akroshko <akroshko.public+github@gmail.com>
2015-12-23 00:25:43 -0600

    Small update replacing ~ with ${HOME}. Made videos and music download to ~/Documents.

--- symlink-home.sh
+++ symlink-home.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 SCRIPTPATH=$(dirname "$0")
 # requires https://github.com/akroshko/bash-stdlib
-source ~/.bash_library
-need_new_directory ~/.conkerorrc
-need_new_symlink ${SCRIPTPATH}/conkerorrc.js ~/.conkerorrc/conkerorrc.js
-need_new_symlink ${SCRIPTPATH}/conkyrc ~/.conkyrc
-need_new_symlink ${SCRIPTPATH}/gitconfig ~/.gitconfig
-need_new_symlink ${SCRIPTPATH}/gitignore ~/.gitignore
-need_new_symlink ${SCRIPTPATH}/guile ~/.guile
-need_new_symlink ${SCRIPTPATH}/hgpersonal ~/.hgpersonal
-need_new_symlink ${SCRIPTPATH}/inputrc ~/.inputrc
-need_new_symlink ${SCRIPTPATH}/latexmkrc ~/.latexmkrc
-need_new_symlink ${SCRIPTPATH}/screenrc ~/.screenrc
+source ${HOME}/.bash_library
+need_new_directory ${HOME}/.conkerorrc
+need_new_symlink ${SCRIPTPATH}/conkerorrc.js ${HOME}/.conkerorrc/conkerorrc.js
+need_new_symlink ${SCRIPTPATH}/conkyrc ${HOME}/.conkyrc
+need_new_symlink ${SCRIPTPATH}/gitconfig ${HOME}/.gitconfig
+need_new_symlink ${SCRIPTPATH}/gitignore ${HOME}/.gitignore
+need_new_symlink ${SCRIPTPATH}/guile ${HOME}/.guile
+need_new_symlink ${SCRIPTPATH}/hgpersonal ${HOME}/.hgpersonal
+need_new_symlink ${SCRIPTPATH}/inputrc ${HOME}/.inputrc
+need_new_symlink ${SCRIPTPATH}/latexmkrc ${HOME}/.latexmkrc
+need_new_symlink ${SCRIPTPATH}/screenrc ${HOME}/.screenrc
 # empty include files
-need_new_symlink /dev/null ~/.xbindkeys_first_personal.scm
-need_new_symlink /dev/null ~/.xbindkeys_second_personal.scm
-need_new_symlink ${SCRIPTPATH}/xbindkeysrc.scm ~/.xbindkeysrc
-need_new_symlink ${SCRIPTPATH}/pentadactylrc ~/.pentadactylrc
+need_new_symlink /dev/null ${HOME}/.xbindkeys_first_personal.scm
+need_new_symlink /dev/null ${HOME}/.xbindkeys_second_personal.scm
+need_new_symlink ${SCRIPTPATH}/xbindkeysrc.scm ${HOME}/.xbindkeysrc
+need_new_symlink ${SCRIPTPATH}/pentadactylrc ${HOME}/.pentadactylrc
 # openbox
-need_new_directory ~/.config
-need_new_directory ~/.config/openbox
-need_new_directory ~/.config/autostart
-need_new_symlink ${SCRIPTPATH}/openbox.xml ~/.config/openbox/rc.xml
-need_new_symlink ${SCRIPTPATH}/openbox-autostart ~/.config/openbox/autostart
+need_new_directory ${HOME}/.config
+need_new_directory ${HOME}/.config/openbox
+need_new_directory ${HOME}/.config/autostart
+need_new_symlink ${SCRIPTPATH}/openbox.xml ${HOME}/.config/openbox/rc.xml
+need_new_symlink ${SCRIPTPATH}/openbox-autostart ${HOME}/.config/openbox/autostart
 # X11
-need_new_symlink ${SCRIPTPATH}/xmodmap ~/.xmodmap
-need_new_symlink ${SCRIPTPATH}/xpdfrc ~/.xpdfrc
-need_new_symlink ${SCRIPTPATH}/Xresources ~/.Xresources
+need_new_symlink ${SCRIPTPATH}/xmodmap ${HOME}/.xmodmap
+need_new_symlink ${SCRIPTPATH}/xpdfrc ${HOME}/.xpdfrc
+need_new_symlink ${SCRIPTPATH}/Xresources ${HOME}/.Xresources
