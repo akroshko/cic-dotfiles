9f3e4c2a4f7f76c5f5a4c711a961e9bd9fd0bfa4
Andrew Kroshko <akroshko.public+devel@gmail.com>
2016-11-15 19:00:33 -0600

    More pi improvements

--- symlink-home.sh
+++ symlink-home.sh
@@ -29,6 +29,10 @@ main () {
     need_new_directory ${HOME}/.themes/MyClearlooks/
     need_new_directory ${HOME}/.themes/MyClearlooks/openbox-3/
     need_new_symlink ${SCRIPTPATH}/openbox.xml       ${HOME}/.config/openbox/rc.xml
+    # raspberry pi openbox
+    if [[ -e "${HOME}"/.config/lxsession/LXDE-pi ]]; then
+        need_new_symlink ${SCRIPTPATH}/lxde-pi-autostart "${HOME}"/.config/lxsession/LXDE-pi/autostart
+    fi
     # TODO: unify eventually?
     need_new_symlink ${SCRIPTPATH}/lxde-pi-rc.xml    ${HOME}/.config/openbox/lxde-pi-rc.xml
     need_new_symlink ${SCRIPTPATH}/openbox-menu.xml  ${HOME}/.config/openbox/menu.xml
