3f8306ca02d86b668bbb6160c2789c85f382dfcf
Andrew Kroshko <akroshko.public+devel@gmail.com>
2017-11-19 13:34:44 -0600

    Some little updates.

--- urxvt-scrollback.pl
+++ urxvt-scrollback.pl
@@ -1,7 +1,30 @@
 #! perl -w
-# Author:   Bert Muennich
-# Website:  http://www.github.com/muennich/urxvt-perls
-# License:  GPLv2
+# Copyright (C) 2015-2016, Andrew Kroshko, Bert Muennich, all rights reserved.
+#
+# Author: Andrew Kroshko
+# Maintainer: Andrew Kroshko <akroshko@gmail.com>
+# Created: Sun Nov 19, 2017
+# Version: 20171119
+# URL: https://github.com/akroshko/dotfiles-stdlib
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
+########################################
+########## Original copyright ##########
+#    Author:   Bert Muennich
+#    Website:  http://www.github.com/muennich/urxvt-perls
+#    License:  GPLv2
 
 # Use keyboard shortcuts to copy the selection to the clipboard and to paste
 # the clipboard contents (optionally escaping all special characters).
