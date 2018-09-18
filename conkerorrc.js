15370fcfcb0886cde74d9e439dbcc84ee8c4d11c
Andrew Kroshko <akroshko.public+devel@gmail.com>
2018-09-18 12:27:41 -0600

    Many small updates.

--- conkerorrc.js
+++ conkerorrc.js
@@ -866,7 +866,7 @@ var current_conkeror_proxy_init=true;
 interactive("fetch-video", "Fetch Video",
     function (I) {
         // TODO: why does --working-directory not work?
-        var cmd_str = 'rxvt-unicode -cd "${HOME}/Documents" -e bash -i -c "youtube-dl --no-cache-dir ' + I.buffer.display_uri_string + ';wait;while read -r -t 0;do read -r; done;read -n 1 -s -r -p \'Press any key to continue...\'"'
+        var cmd_str = 'rxvt-unicode -cd "$HOME/Documents" -e bash -i -c "youtube-dl --no-cache-dir ' + I.buffer.display_uri_string + ';wait;while read -r -t 0;do read -r; done;read -n 1 -s -r -p \'Press any key to continue...\'"'
         shell_command_blind(cmd_str);
     });
 define_key(content_buffer_normal_keymap,"C-c v","fetch-video");
@@ -876,7 +876,7 @@ interactive("fetch-video-as-audio", "Fetch Video as audio",
     function (I) {
         // TODO: why does --working-directory not work?
         // TODO: default format
-        var cmd_str = 'rxvt-unicode -cd "${HOME}/Documents" -e bash -i -c "youtube-dl --no-cache-dir --extract-audio --audio-format mp3 --audio-quality 9 ' + I.buffer.display_uri_string + ';wait;while read -r -t 0; do read -r; done;read -n 1 -s -r -p \'Press any key to continue...\'"'
+        var cmd_str = 'rxvt-unicode -cd "$HOME/Documents" -e bash -i -c "youtube-dl --no-cache-dir --extract-audio --audio-format mp3 --audio-quality 9 ' + I.buffer.display_uri_string + ';wait;while read -r -t 0; do read -r; done;read -n 1 -s -r -p \'Press any key to continue...\'"'
         shell_command_blind(cmd_str);
     });
 define_key(content_buffer_normal_keymap,"C-c V","fetch-video-as-audio");
@@ -885,7 +885,7 @@ interactive("fetch-video-playlist-as-audio", "Fetch Video playlist as audio",
     function (I) {
         // TODO: why does --working-directory not work?
         // TODO: default format
-        var cmd_str = 'rxvt-unicode -cd "${HOME}/Documents" -e bash -i -c "youtube-dl --no-cache-dir --extract-audio --output \'%(playlist_title)s/%(playlist_title)s-%(playlist_index)s-%(title)s-%(id)s.%(ext)s\' --audio-format mp3 --audio-quality 9 ' + I.buffer.display_uri_string + ';wait;while read -r -t 0; do read -r; done;read -n 1 -s -r -p \'Press any key to continue...\'"'
+        var cmd_str = 'rxvt-unicode -cd "$HOME/Documents" -e bash -i -c "youtube-dl --no-cache-dir --extract-audio --output \'%(playlist_title)s/%(playlist_title)s-%(playlist_index)s-%(title)s-%(id)s.%(ext)s\' --audio-format mp3 --audio-quality 9 ' + I.buffer.display_uri_string + ';wait;while read -r -t 0; do read -r; done;read -n 1 -s -r -p \'Press any key to continue...\'"'
         shell_command_blind(cmd_str);
     });
 define_key(content_buffer_normal_keymap,"C-c M-v","fetch-video-playlist-as-audio");
--- symlink-home.sh
+++ symlink-home.sh
@@ -2,71 +2,71 @@
 SCRIPTPATH=$(dirname $(readlink -f "$0"))
 main () {
     # requires https://github.com/akroshko/bash-stdlib
-    source "${HOME}/.bash_library"
-    need_new_directory "${HOME}/.conkerorrc"
+    source "$HOME/.bash_library"
+    need_new_directory "$HOME/.conkerorrc"
     # make sure the normal conkerorrc is loaded first
-    need_new_symlink "${SCRIPTPATH}/conkerorrc.js"          "${HOME}/.conkerorrc/00conkerorrc.js"
+    need_new_symlink "${SCRIPTPATH}/conkerorrc.js"          "$HOME/.conkerorrc/00conkerorrc.js"
     # add some garbage collection to conkeror startup
-    echo "Cu.forceGC();" >                                  "${HOME}/.conkerorrc/zzconkerorgc.js"
+    echo "Cu.forceGC();" >                                  "$HOME/.conkerorrc/zzconkerorgc.js"
     # TODO: add a fence here to make sure this works
-    cp "${SCRIPTPATH}/conkeror-webpage-key-kill.js"         "${HOME}/cic-external-vcs/conkeror-external/modules/page-modes/webpage-key-kill.js"
-    need_new_symlink "${SCRIPTPATH}/conkeror-webjumps.json" "${HOME}/.conkerorrc/conkeror-webjumps.json"
-    need_new_symlink "${SCRIPTPATH}/conkyrc"                "${HOME}/.conkyrc"
-    need_new_symlink "${SCRIPTPATH}/gitconfig"              "${HOME}/.gitconfig"
-    need_new_symlink "${SCRIPTPATH}/gitignore"              "${HOME}/.gitignore"
-    need_new_symlink "${SCRIPTPATH}/guile"                  "${HOME}/.guile"
-    need_new_symlink "${SCRIPTPATH}/inputrc"                "${HOME}/.inputrc"
-    need_new_symlink "${SCRIPTPATH}/latexmkrc"              "${HOME}/.latexmkrc"
-    need_new_symlink "${SCRIPTPATH}/screenrc"               "${HOME}/.screenrc"
-    need_new_symlink "${SCRIPTPATH}/zile"                   "${HOME}/.zile"
-    need_new_symlink "${SCRIPTPATH}/pentadactylrc"          "${HOME}/.pentadactylrc"
+    cp "${SCRIPTPATH}/conkeror-webpage-key-kill.js"         "$HOME/cic-external-vcs/conkeror-external/modules/page-modes/webpage-key-kill.js"
+    need_new_symlink "${SCRIPTPATH}/conkeror-webjumps.json" "$HOME/.conkerorrc/conkeror-webjumps.json"
+    need_new_symlink "${SCRIPTPATH}/conkyrc"                "$HOME/.conkyrc"
+    need_new_symlink "${SCRIPTPATH}/gitconfig"              "$HOME/.gitconfig"
+    need_new_symlink "${SCRIPTPATH}/gitignore"              "$HOME/.gitignore"
+    need_new_symlink "${SCRIPTPATH}/guile"                  "$HOME/.guile"
+    need_new_symlink "${SCRIPTPATH}/inputrc"                "$HOME/.inputrc"
+    need_new_symlink "${SCRIPTPATH}/latexmkrc"              "$HOME/.latexmkrc"
+    need_new_symlink "${SCRIPTPATH}/screenrc"               "$HOME/.screenrc"
+    need_new_symlink "${SCRIPTPATH}/zile"                   "$HOME/.zile"
+    need_new_symlink "${SCRIPTPATH}/pentadactylrc"          "$HOME/.pentadactylrc"
     # .config
-    need_new_directory "${HOME}/.config"
-    need_new_directory "${HOME}/.config/autostart"
+    need_new_directory "$HOME/.config"
+    need_new_directory "$HOME/.config/autostart"
     # openbox
-    need_new_directory "${HOME}/.config/openbox"
-    need_new_directory "${HOME}/.themes"
-    need_new_directory "${HOME}/.themes/MyClearlooks/"
-    need_new_directory "${HOME}/.themes/MyClearlooks/openbox-3/"
-    need_new_symlink ${SCRIPTPATH}/openbox.xml            "${HOME}/.config/openbox/rc.xml"
+    need_new_directory "$HOME/.config/openbox"
+    need_new_directory "$HOME/.themes"
+    need_new_directory "$HOME/.themes/MyClearlooks/"
+    need_new_directory "$HOME/.themes/MyClearlooks/openbox-3/"
+    need_new_symlink ${SCRIPTPATH}/openbox.xml            "$HOME/.config/openbox/rc.xml"
     # raspberry pi openbox
-    [[ -e "${HOME}/.config/lxsession/LXDE-pi" ]] && need_new_symlink ${SCRIPTPATH}/lxde-pi-autostart "${HOME}/.config/lxsession/LXDE-pi/autostart"
+    [[ -e "$HOME/.config/lxsession/LXDE-pi" ]] && need_new_symlink ${SCRIPTPATH}/lxde-pi-autostart "$HOME/.config/lxsession/LXDE-pi/autostart"
     # TODO: unify eventually?
-    need_new_symlink "${SCRIPTPATH}/lxde-pi-rc.xml"               "${HOME}/.config/openbox/lxde-pi-rc.xml"
-    need_new_symlink "${SCRIPTPATH}/openbox-menu.xml"             "${HOME}/.config/openbox/menu.xml"
-    need_new_symlink "${SCRIPTPATH}/openbox-autostart"            "${HOME}/.config/openbox/autostart"
-    need_new_symlink "${SCRIPTPATH}/openbox-myclearlooks.themerc" "${HOME}/.themes/MyClearlooks/openbox-3/themerc"
+    need_new_symlink "${SCRIPTPATH}/lxde-pi-rc.xml"               "$HOME/.config/openbox/lxde-pi-rc.xml"
+    need_new_symlink "${SCRIPTPATH}/openbox-menu.xml"             "$HOME/.config/openbox/menu.xml"
+    need_new_symlink "${SCRIPTPATH}/openbox-autostart"            "$HOME/.config/openbox/autostart"
+    need_new_symlink "${SCRIPTPATH}/openbox-myclearlooks.themerc" "$HOME/.themes/MyClearlooks/openbox-3/themerc"
     # pcmanfm
     # XXXX: overwrite to avoid my git repo being edited by program
-    need_new_directory "${HOME}/.config/pcmanfm/default"
+    need_new_directory "$HOME/.config/pcmanfm/default"
     # XXXX: using backslash in front of cp does not work well on some systems I have for unknown reasons
-    cp_if_different "${SCRIPTPATH}/pcmanfm.conf" "${HOME}/.config/pcmanfm/default/pcmanfm.conf"
+    cp_if_different "${SCRIPTPATH}/pcmanfm.conf" "$HOME/.config/pcmanfm/default/pcmanfm.conf"
     # X11
-    need_new_symlink "${SCRIPTPATH}/xmodmaprc"              "${HOME}/.xmodmaprc"
-    need_new_symlink "${SCRIPTPATH}/xmodmaprc_numpad_fkeys" "${HOME}/.xmodmaprc_numpad_fkeys"
-    need_new_symlink "${SCRIPTPATH}/xpdfrc"                 "${HOME}/.xpdfrc"
-    need_new_symlink "${SCRIPTPATH}/Xresources"             "${HOME}/.Xresources"
+    need_new_symlink "${SCRIPTPATH}/xmodmaprc"              "$HOME/.xmodmaprc"
+    need_new_symlink "${SCRIPTPATH}/xmodmaprc_numpad_fkeys" "$HOME/.xmodmaprc_numpad_fkeys"
+    need_new_symlink "${SCRIPTPATH}/xpdfrc"                 "$HOME/.xpdfrc"
+    need_new_symlink "${SCRIPTPATH}/Xresources"             "$HOME/.Xresources"
     # feh
-    need_new_directory                                      "${HOME}/.config/feh"
-    need_new_symlink "${SCRIPTPATH}/feh-buttons"            "${HOME}/.config/feh/buttons"
-    need_new_symlink "${SCRIPTPATH}/feh-keys"               "${HOME}/.config/feh/keys"
+    need_new_directory                                      "$HOME/.config/feh"
+    need_new_symlink "${SCRIPTPATH}/feh-buttons"            "$HOME/.config/feh/buttons"
+    need_new_symlink "${SCRIPTPATH}/feh-keys"               "$HOME/.config/feh/keys"
     # calibre
     # TODO: copying to avoid settings overwriting.... need to suppress this script if experimenting
-    need_new_directory                           "${HOME}/.config/calibre/shortcuts"
-    # [[ -e ${HOME}/.config/calibre/shortcuts/viewer.plist ]] && rm ${HOME}/.config/calibre/shortcuts/viewer.plist
-    [[ -d "${HOME}/.config/calibre/shortcuts" ]]              && cp_if_different "${SCRIPTPATH}/calibre-viewer.plist" "${HOME}/.config/calibre/shortcuts/viewer.plist"
-    [[ -e "${HOME}/.config/calibre/viewer.py" ]]              && rm                                                   "${HOME}/.config/calibre/viewer.py"
-    [[ -d "${HOME}/.config/calibre/" ]]                       && cp_if_different "${SCRIPTPATH}/calibre-viewer.py"    "${HOME}/.config/calibre/viewer.py"
+    need_new_directory                           "$HOME/.config/calibre/shortcuts"
+    # [[ -e $HOME/.config/calibre/shortcuts/viewer.plist ]] && rm $HOME/.config/calibre/shortcuts/viewer.plist
+    [[ -d "$HOME/.config/calibre/shortcuts" ]]              && cp_if_different "${SCRIPTPATH}/calibre-viewer.plist" "$HOME/.config/calibre/shortcuts/viewer.plist"
+    [[ -e "$HOME/.config/calibre/viewer.py" ]]              && rm                                                   "$HOME/.config/calibre/viewer.py"
+    [[ -d "$HOME/.config/calibre/" ]]                       && cp_if_different "${SCRIPTPATH}/calibre-viewer.py"    "$HOME/.config/calibre/viewer.py"
     # create some quick links
-    need_new_directory "${HOME}/bin"
+    need_new_directory "$HOME/bin"
     # TODO: do better, works for now
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/launch-emacsclient"         "${HOME}/bin/launch-emacsclient"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/launch-emacsclient-fail.sh" "${HOME}/bin/launch-emacsclient-fail.sh"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-focus.sh"         "${HOME}/bin/web-video-focus.sh"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-focus-restore.sh" "${HOME}/bin/web-video-focus-restore.sh"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-minimize.sh"      "${HOME}/bin/web-video-minimize.sh"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-pause-toggle.sh"  "${HOME}/bin/web-video-pause-toggle.sh"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-restore.sh"       "${HOME}/bin/web-video-restore.sh"
-    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/zathura-local.sh"           "${HOME}/bin/zathura-local.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/launch-emacsclient"         "$HOME/bin/launch-emacsclient"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/launch-emacsclient-fail.sh" "$HOME/bin/launch-emacsclient-fail.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-focus.sh"         "$HOME/bin/web-video-focus.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-focus-restore.sh" "$HOME/bin/web-video-focus-restore.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-minimize.sh"      "$HOME/bin/web-video-minimize.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-pause-toggle.sh"  "$HOME/bin/web-video-pause-toggle.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/web-video-restore.sh"       "$HOME/bin/web-video-restore.sh"
+    need_new_symlink "${SCRIPTPATH}/../bash-stdlib/zathura-local.sh"           "$HOME/bin/zathura-local.sh"
 }
 main
